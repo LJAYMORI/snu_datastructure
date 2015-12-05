59120db0e5d6018a508e742c954d7dc1c23aee4f
Hansol Jang <zzang.hansol@gmail.com>
2015-12-05 18:51:18 +0900

    12/5(한솔) - 우선순위 큐

---
+++ main/datastructure/queue/priorityqueue.c
@@ -0,0 +1,22 @@
+#include "priorityheap.h"
+#include "./heap/heap.h"
+
+void PQueueInit(PQueue * ppq, PriorityComp pc)
+{
+	HeapInit(ppq, pc);
+}
+
+int PQIsEmpty(PQueue * ppq)
+{
+	return HIsEmpty(ppq);
+}
+
+void PEnqueue(PQueue * ppq, PQData data)
+{
+	HInsert(ppq, data);
+}
+
+PQData PDequeue(PQueue * ppq)
+{
+	return HDelete(ppq);
+}
