525b23e0ba4cb71dc590fcec35fe6e00d3d5528a
Kim Tae Nyun <newgto2001@gmail.com>
2015-12-05 20:54:53 +0900

    ..

--- main/algorithm/151205(kruscal)/tee/kruscal.c
+++ main/algorithm/151205(kruscal)/tee/kruscal.c
@@ -5,3 +5,44 @@
 #include "../kruscal.h"
 
 
+// 그래프의 초기화
+void GraphInit(ALGraph * pg, int nv)
+{
+
+}
+
+// 그래프의 리소스 해제
+void GraphDestroy(ALGraph * pg)
+{
+
+}
+
+// 간선의 추가
+void AddEdge(ALGraph * pg, int fromV, int toV, int weight)
+{
+
+}
+
+// 간선의 정보 출력
+void ShowGraphEdgeInfo(ALGraph * pg)
+{
+
+}
+
+// Depth First Search: 정점의 정보 출력
+void DFShowGraphVertex(ALGraph * pg, int startV)
+{
+
+}
+
+// 크루스칼 최소 비용 신장 트리의 구성
+void ConKruskalMST(ALGraph * pg)
+{
+
+}
+
+// 간선의 가중치 정보 출력
+void ShowGraphEdgeWeightInfo(ALGraph * pg)
+{
+
+}
