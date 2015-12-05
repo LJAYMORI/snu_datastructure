2c15f05a1776f41e6237eee489995a3e079d462b
Hansol Jang <zzang.hansol@gmail.com>
2015-12-05 15:59:30 +0900

    12/5(한솔) - binary tree 수정

--- main/datastructure/tree/binarytree.h
+++ main/datastructure/tree/binarytree.h
@@ -1,5 +1,5 @@
-#ifndef __BINARY_TREE_H__
-#define __BINARY_TREE_H__
+#ifndef __BINARY_TREE3_H__
+#define __BINARY_TREE3_H__
 
 typedef int BTData;
 
@@ -10,7 +10,6 @@ typedef struct _bTreeNode
 	struct _bTreeNode * right;
 } BTreeNode;
 
-/*** BTreeNode 관련 연산들 ****/
 BTreeNode * MakeBTreeNode(void);
 BTData GetData(BTreeNode * bt);
 void SetData(BTreeNode * bt, BTData data);
@@ -20,5 +19,23 @@ BTreeNode * GetRightSubTree(BTreeNode * bt);
 
 void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
 void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+
+// 왼쪽 자식 노드 제거, 제거된 노드의 주소 값이 반환된다.
+BTreeNode * RemoveLeftSubTree(BTreeNode * bt);
+
+// 오른쪽 자식 노드 제거, 제거된 노드의 주소 값이 반환된다.
+BTreeNode * RemoveRightSubTree(BTreeNode * bt);
+
+// 메모리 소멸을 수반하지 않고 main의 왼쪽 자식 노드를 변경한다. 
+void ChangeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+
+// 메모리 소멸을 수반하지 않고 main의 오른쪽 자식 노드를 변경한다. 
+void ChangeRightSubTree(BTreeNode * main, BTreeNode * sub);
 
 #endif
