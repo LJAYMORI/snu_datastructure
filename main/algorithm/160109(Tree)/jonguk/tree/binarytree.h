a11633587570fe91c194fece738a80a2f2fbe262
Kim Tae Nyun <newgto2001@gmail.com>
2016-01-09 15:36:22 +0900

    트리 2

--- main/algorithm/160109(Tree)/jonguk/tree/binarytree.h
+++ main/algorithm/160109(Tree)/jonguk/tree/binarytree.h
@@ -1,9 +1,6 @@
 #ifndef __BINARY_TREE3_H__
 #define __BINARY_TREE3_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef int BTData;
 
 typedef struct _bTreeNode
@@ -22,5 +19,12 @@ BTreeNode * GetRightSubTree(BTreeNode * bt);
 
 void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
 void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+
 
 #endif
