86a4d0b497cd7429845aec721bf9d5fdda45e2c0
Kim Tae Nyun <newgto2001@gmail.com>
2016-01-09 15:34:28 +0900

    트리 2

--- main/algorithm/160109(Tree)/tee/tree/BinaryTreeMain.c
+++ main/algorithm/160109(Tree)/tee/tree/BinaryTreeMain.c
@@ -8,23 +8,37 @@ int main(void)
     BTreeNode * bt2 = MakeBTreeNode();
     BTreeNode * bt3 = MakeBTreeNode();
     BTreeNode * bt4 = MakeBTreeNode();
+    BTreeNode * bt5 = MakeBTreeNode();
+    BTreeNode * bt6 = MakeBTreeNode();
 
     SetData(bt1, 1);
     SetData(bt2, 2);
     SetData(bt3, 3);
     SetData(bt4, 4);
+    SetData(bt5, 5);
+    SetData(bt6, 6);
 
     MakeLeftSubTree(bt1, bt2);
     MakeRightSubTree(bt1, bt3);
     MakeLeftSubTree(bt2, bt4);
+    MakeRightSubTree(bt2, bt5);
+    MakeRightSubTree(bt3, bt6);
 
-    printf("%d \n", GetData(GetLeftSubTree(bt1)));
-
-    printf("%d \n", GetData(GetLeftSubTree(GetLeftSubTree(bt1))));
-
+    PreorderTraverse(bt1, ShowIntData);
+    printf("\n");
+    InorderTraverse(bt1, ShowIntData);
+    printf("\n");
+    PostorderTraverse(bt1, ShowIntData);
+    printf("\n");
     return 0;
 
     // 예상 출력
-    // 2
-    // 4
+    // 1 2 4 5 3 6
+    // 4 2 5 1 3 6
+    // 4 5 2 6 3 1
+}
+
+void ShowIntData(int data)
+{
+    printf("%d ", data);
 }
--- main/algorithm/160109(Tree)/tee/tree/binarytree.h
+++ main/algorithm/160109(Tree)/tee/tree/binarytree.h
@@ -20,4 +20,11 @@ BTreeNode * GetRightSubTree(BTreeNode * bt);
 void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
 void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
 
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+
+
 #endif
